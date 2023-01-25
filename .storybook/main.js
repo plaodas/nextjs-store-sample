62edeb1e25b5b7772021c06da9576f139ed0aa28
Hisanaga Agake <agake@plao-das.com>
2023-01-25 13:44:29 +0900

    set up storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,22 +2,22 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 const path = require('path')
 
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-postcss",
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-postcss',
   ],
   staticDirs: ['public'],
   babel: async options => ({
     ...options,
     plugins: [
       '@babel/plugin-proposal-class-properties',
-      '@babel/plugin-proposal-private',
+      '@babel/plugin-proposal-private-methods',
       '@babel/plugin-proposal-private-property-in-object',
     ],
   }),
@@ -30,8 +30,8 @@ module.exports = {
 
     return config
   },
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  'framework': '@storybook/react',
+  'core': {
+    'builder': '@storybook/builder-webpack5'
   },
 }
